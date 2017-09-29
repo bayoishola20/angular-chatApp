f8bc7ca5972b234fa164d86adf1ceb462bf8fb27
bayoishola20 <bayoishola20@yahoo.com>
2017-09-29 23:46:37 +0100

    socket server client communication

--- angular-src/src/app/home/home.component.ts
+++ angular-src/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client'; //library defined in server.jss
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  socket: io.Socket
+  
+  constructor() {
+    this.socket = io.connect();
+   }
 
   ngOnInit() {
+    this.socket.emit('event1', {
+      msg: 'Client to server, are you listening...'
+    });
+    this.socket.on('event2', (data: any) => {
+      console.log(data.msg);
+      this.socket.emit('event3', {
+        msg: 'Yes, it works for me!!!'
+      });
+    });
+    this.socket.on('event4', (data:any) => {
+      console.log(data.msg);
+    });
   }
 
 }
